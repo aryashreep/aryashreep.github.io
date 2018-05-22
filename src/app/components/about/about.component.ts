5d74d87b87ab579f21b112276117e20045fa7e09
Aryashree Pritikrishna <aryashree@etechbuddy.com>
2018-05-22 09:53:19 +0530

    Fixes for lint issue

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component , OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ArticlesService } from '../../service/articles.service';
 import { Articles } from '../../interface/articles';
@@ -12,19 +12,19 @@ import { Articles } from '../../interface/articles';
 export class AboutComponent implements OnInit {
   title = "Arya's portfolio :: eTechBuddy";
   _articlesArray: Articles[];
-  
+
   constructor(private titleService: Title, private articlesSerivce: ArticlesService) { }
 
-  getArticles(): void{
-	this.articlesSerivce.getArticles()
-	  .subscribe(
-		resultArray => this._articlesArray = resultArray,
-		error => console.log('Error :: ' + error)
-	  )
-  }  
-  
+  getArticles(): void {
+    this.articlesSerivce.getArticles()
+      .subscribe(
+        resultArray => this._articlesArray = resultArray,
+        error => console.log('Error :: ' + error)
+      )
+  }
+
   ngOnInit() {
-	  this.titleService.setTitle(this.title);
-	  this.getArticles();
+    this.titleService.setTitle(this.title);
+    this.getArticles();
   }
 }
--- src/app/service/articles.service.ts
+++ src/app/service/articles.service.ts
@@ -7,19 +7,19 @@ import { Articles } from '../interface/articles';
 @Injectable()
 export class ArticlesService {
   private _articlesURL = 'http://www.etechbuddy.com//assets/data/api/articles.json';
-  constructor( private http: Http) {  
+  constructor(private http: Http) {
   }
-  
+
   getArticles(): Observable<Articles[]> {
     return this.http
-	  .get(this._articlesURL)
-	  .map((response: Response) => {
-		  return <Articles[]>response.json();
-	  })
-	  .catch(this.handleError);
+      .get(this._articlesURL)
+      .map((response: Response) => {
+        return <Articles[]>response.json();
+      })
+      .catch(this.handleError);
   }
-  
+
   private handleError(errror: Response) {
-	  return Observable.throw(errror.statusText);
-  }  
+    return Observable.throw(errror.statusText);
+  }
 }
